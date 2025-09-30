39e9033e9826d728c2ddb01d89c2cc027a5b5791
jramos <jramos@intercommerce.com.ph>
2025-09-30 10:20:46 +0800

    [add] update struts

--- WEB-INF/classes/com/ins/registration/web/ProfilesController.java
+++ WEB-INF/classes/com/ins/registration/web/ProfilesController.java
@@ -182,18 +182,17 @@ public class ProfilesController extends BaseController implements Preparable, Va
             }
         }
 
-        log.debug(">>> action name: " + this.getActionName());
-        log.debug(">>> role: " + this.getRole());
-        log.debug(">>> clientType: "  + this.getClientType());
-        log.debug(">>> 1st condition: "+ (this.getActionName().equals("profileForm")));
-        log.debug(">>> 2nd condition: " + ("2".equals(getRole()) && !"BR".equals(this.clientType)));
         if(this.getActionName().equals("profileForm")){
-            log.debug(">>> inside 1st condition ");
             if("2".equals(getRole()) && !"BR".equals(this.clientType)){
-                log.debug(">>> inside 2nd condition ");
+                log.debug(">>> Blocking add profile action for broker accounts. Condition not met.");
+                log.debug(">>> action name: " + this.getActionName());
+                log.debug(">>> role: " + this.getRole());
+                log.debug(">>> clientType: "  + this.getClientType());
+                log.debug(">>> 1st condition: "+ (this.getActionName().equals("profileForm")));
+                log.debug(">>> 2nd condition: " + ("2".equals(getRole()) && !"BR".equals(this.clientType)));
                 this.addFieldError("clientType", "Action not allowed. Your account is\n" +
                         "registered as a Broker. Adding other client types is not\n" +
-                        "permitted for this account type.”");
+                        "permitted for this account type.");
             }
         }
 
